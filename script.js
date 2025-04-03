2d9b1688afa512b56b73957174eb3942b7d06e19
Rainbowcats999 <526erizov@frhsd.com>
2025-04-03 16:34:20 +0000

    Still to upload the five backgrounds

---
+++ script.js
@@ -0,0 +1,16 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = canvas.width = 800;
+const CANVAS_HEIGHT = canvas.height = 700;
+let gameSpeed = 5;
+
+const backgroundLayer1 =  new Image();
+backgroundLayer1.src = 'layer-1.png';
+const backgroundLayer1 =  new Image();
+backgroundLayer1.src = 'layer-1.png';
+const backgroundLayer1 =  new Image();
+backgroundLayer1.src = 'layer-1.png';
+const backgroundLayer1 =  new Image();
+backgroundLayer1.src = 'layer-1.png';
+const backgroundLayer1 =  new Image();
+backgroundLayer1.src = 'layer-1.png';
