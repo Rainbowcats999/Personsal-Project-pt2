fdcd5673730c0432dbbf9d341865fed3f12f1ad0
Rainbowcats999 <526erizov@frhsd.com>
2025-04-22 16:34:31 +0000

    IMAGES STILL DONT WANNA WORK UGHHH

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGHT = canvas.height = 700;
-let gameSpeed = 5;
+let gameSpeed = 10;
 
 const backgroundLayer1 =  new Image();
 backgroundLayer1.src = 'layer-1.png';
@@ -15,13 +15,22 @@ backgroundLayer4.src = 'layer-4.png';
 const backgroundLayer5 =  new Image();
 backgroundLayer5.src = 'layer-5.png';
 
-let x = 0;
+class Layer {
+    constructor(image, speedModifier){
+        this.x = 0;
+        this.y = 0;
+        this.width = 2400;
+        this.height = 700;
+        this.x2 = this.width;
+        this.image = image;
+        this.speedModifier =  speedModifier;
+        this.speed = gameSpeed;
+    }
+}
 
-function animate() {
-    ctx.clearRect(0, 0,)
-    ctx.drawImage(backgroundLayer4, x, 0);
-    x--;
-    requestAnimationFrame(animate);
+function animate(){
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    requestAnimationFrame(animate); 
 };
 
 animate();
